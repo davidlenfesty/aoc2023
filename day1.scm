7e7e4d1d43e09fba2ce323bdcb57e45274777f07
David Lenfesty <lenfesty@ualberta.ca>
2023-12-05 18:23:54 -0700

    rework to use string offset instead of character list

--- day1.scm
+++ day1.scm
@@ -1,44 +1,31 @@
-;;; Read all lines from stdin and print them back
-(define readlines
-  (lambda (line)
-    (cond
-      [(eof-object? line) #f]
-      [(null? line) (begin
-        (display "null input")
-        (readlines (get-line (current-input-port)))
-      )]
-      [#t (begin
-        (display "uh")
-        (display (string-append line "\n"))
-        (readlines (get-line (current-input-port)))
-      )]
-    )
-  )
-)
-
+;;; Finds the last digit in a string
 (define last-digit
-  (lambda (str . digit)
-    (cond
-      ; No string left and no digit found
-      [(and (null? str) (null? digit))
-        (error "last-digit" "no digit found" str)]
-      ; No string left and a digit was found
-      [(null? str) (car digit)]
-      ; Input was a digit, set digit when we recurse
-      [(char-numeric? (car str))
-        (last-digit (cdr str) (car str))]
-      ; Input not a digit, no digit found already
-      [(null? digit) (last-digit (cdr str))]
-      ; Input not a digit, digit found
-      [#t (last-digit (cdr str) (car digit))]
+  (lambda (str i . digit)
+    (let ((len (string-length str)))
+      (cond
+        ; No string left to parse
+        [(eq? len i) (if (null? digit)
+          ; digit not found 
+          (error "last-digit" "no digit found" str)
+          ; digit found
+          (car digit)
+        )]
+        ; Input was a digit, set digit when we recurse
+        [(char-numeric? (string-ref str i))
+          (last-digit str (+ i 1) (string-ref str i))]
+        ; Input not a digit, no digit found already
+        [(null? digit) (last-digit str (+ i 1))]
+        ; Input not a digit, digit found already
+        [#t (last-digit str (+ i 1) (car digit))]
 
 
-      ; TODO why doesn't this work? fails with 'attempt to apply non-procedure'
-      ; I'm trying to create a list with the first element being the list of remaining characters,
-      ; because digit is either null (an empty list) or a list with a digit
-      ;[#t (begin
-      ;  (display "iter1")
-      ;  (apply last-digit (append (remainder) digit)))]
+        ; TODO why doesn't this work? fails with 'attempt to apply non-procedure'
+        ; I'm trying to create a list with the first element being the list of remaining characters,
+        ; because digit is either null (an empty list) or a list with a digit
+        ;[#t (begin
+        ;  (display "iter1")
+        ;  (apply last-digit (append (remainder) digit)))]
+    )
     )))
 
 ;;; Returns a pair of the first and last digit in the provided string
@@ -46,16 +33,15 @@
 ;;; Note: the last digit can also be the first digit
 (define first-and-last-digit
   (lambda (str)
-    (let [(inner (lambda (inner str)
-      (cond
-        [(char-numeric? (car str))
-          (list (car str) (last-digit str))]
-        ; Could have been an if
-        [#t (inner inner (cdr str))]
+    (let [(inner (lambda (inner str i)
+      (if (char-numeric? (string-ref str i))
+        (list (string-ref str i) (last-digit str i))
+        (inner inner str (+ i 1))
       )))]
-      ; Apply our implementation after turning the string to a list
-      (inner inner (string->list str)))))
+      ; Apply our implementation
+      (inner inner str 0))))
 
+;;; Converts a pair of first and last digit into an integer
 (define get-string-number
   (lambda (str)
     (string->number (apply string (first-and-last-digit str)))))
@@ -68,4 +54,25 @@
       (begin
       (self self (+ x (get-string-number line))))
     ))))]
-  (display (string-append (number->string (readline readline 0)) "\n")))
+  (display (string-append "Part 1 answer: " (number->string (readline readline 0)) "\n")))
+
+;;; Returns the number at the "current" position in the "string" (list of characters), or false
+(define get-number-at-position
+  (lambda (str)
+    (if (char-numeric? (car str))
+      (car str)
+      (let [
+        (substitutions [
+          ("one" #\1)
+          ("two" #\2)
+          ("three" #\3)
+          ("four" #\4)
+          ("five" #\5)
+          ("six" #\6)
+          ("seven" #\7)
+          ("eight" #\8)
+          ("nine" #\9)
+          ("zero" #\0)])]
+
+        (let [()])
+    ))))
